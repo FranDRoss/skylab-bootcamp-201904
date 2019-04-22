bb86ffa1fb0a8d54ca5aaed98556ea8d245c2250
Fran Destruels <frand.ross@icloud.com>
2019-04-22 19:49:35 +0200

    first commit

---
+++ staff/francesc-destruels/duck-app/scripts/components/Home.js
@@ -0,0 +1,9 @@
+
+
+function Home(props) {
+    return <main>
+        <h1>Hello, {props.name}!</h1>
+    </main>
+}
+
+export default Home
---
+++ staff/francesc-destruels/duck-app/scripts/main.jsx
@@ -0,0 +1,53 @@
+const { Component } = React
+
+//IRA AL COMPONENTE LOGIN
+function Login(props) {
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const username = event.target.username.value
+        const password = event.target.password.value
+
+        props.onLogin(username, password)
+    }
+
+    return <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password" placeholder="password" />
+        <button>Login</button>
+        <span>{props.error}</span>
+    </form>
+}
+
+//IRA AL COMPONENTE HOME
+function Home(props) {
+    return <main>
+        <h1>Hello, {props.name}!</h1>
+    </main>
+}
+
+
+//MAIN WEB
+class App extends Component {
+    state = { loggedIn: false, loginError: null, userName: null }
+
+    handleLogin = (username, password) => {
+        const loggedIn = logic.loginUser(username, password)
+
+        // if (!loggedIn)
+        //     this.setState({ loginError: 'wrong credentials' })
+        // else 
+        //     this.setState({ loggedIn })
+
+        this.setState(loggedIn ? { loggedIn, userName: logic.retrieveUser().name } : { loginError: 'wrong credentials' })
+    }
+
+    render() {
+        return [
+            !this.state.loggedIn && <Login onLogin={this.handleLogin} error={this.state.loginError} />,
+            this.state.loggedIn && <Home name={this.state.userName} />
+        ]
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
