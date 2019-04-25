fc4d10f6e867eb4962cd963dcedd0e14ac4a2809
Fran Destruels <frand.ross@icloud.com>
2019-04-25 16:33:51 +0200

    cambios de manu

---
+++ staff/francesc-destruels/duck-app/src/components/Home/literals.js
@@ -0,0 +1,18 @@
+export default  {
+    en: {
+        hello: 'Welcome Hell,',
+        logout: 'Logout'
+    },
+    es: {
+        hello: 'Bienvenido al Infierno,',
+        logout: 'Cerrar sesión'
+    },
+    ca: {
+        hello: 'Benvingut al infer,',
+        logout: 'Tanca sessió'
+    },
+    ga: {
+        hello: 'Inferno',
+        logout: 'Finalizar sesión'
+    }
+}
