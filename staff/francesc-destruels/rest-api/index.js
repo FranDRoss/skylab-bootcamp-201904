65c43e93687c59904badfce1313f7c43df710c82
Fran Destruels <frand.ross@icloud.com>
2019-05-14 14:32:13 +0200

    change

--- staff/francesc-destruels/rest-api/index.js
+++ staff/francesc-destruels/rest-api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const package = require('./package.json')
 const bodyParser = require('body-parser')
 const logic = require('./logic')
+const handleErrors = require('./routes/handle-errors')
 
 const jsonParser = bodyParser.json()
 
@@ -11,50 +12,35 @@ const app = express()
 
 app.post('/user', jsonParser, (req, res) => { // ruta simple user para registra, devuelve un Ok.
     const { body: { name, surname, email, password } } = req
-    
-    try {
+
+    handleErrors(() =>
         logic.registerUser(name, surname, email, password)
-            .then(() => res.json({ token }))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
+            .then(() => res.status(201).json({ message: 'Ok, user registered. ' })),
+        res)
 })
 
 app.post('/auth', jsonParser, (req, res) => { //  Ruta auth para authnticar el user devolverá token
     const { body: { email, password } } = req
-    
-    try {
-        logic.authenticateUser(email, password) // cojerá token
-            .then(token => res.json({ token }))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
+
+    handleErrors(() =>
+        logic.authenticateUser(email, password)
+            .then(token => res.json({ token })),
+        res)
 })
 
 app.get('/user', jsonParser, (req, res) => { // Un get + ruta user y ID para obtener datos de usuario
     let { headers: { authorization: token } } = req
     token = token.split(' ')[1]
-    
-    try {
-        logic.retrieveUser(token) // cojerá token
-            .then(({ name, surname, email }) => res.json({ name, surname, email }))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
+
+    handleErrors(() =>
+        logic.retrieveUser(token)
+            .then(({ name, surname, email }) => res.json({ name, surname, email })),
+        res)
 })
 
 // app.put('/user', jsonParser, (req, res) => { Necesita el token más el objeto para actualizar.
 //     const { body: { token, updatedInfo } } = req
-    
+
 //     try {
 //         logic.updateUser(token, updatedInfo) // cojerá token
 //             .then(() => res.json({ message: 'Ok, user registered. '}))
@@ -67,63 +53,43 @@ app.get('/user', jsonParser, (req, res) => { // Un get + ruta user y ID para obt
 // })
 
 app.get('/duckSearch', jsonParser, (req, res) => { // Un get + ruta search
-    let { headers: { authorization: token }, query: { q:query } } = req
+    let { headers: { authorization: token }, query: { q: query } } = req
     token = token.split(' ')[1]
-    
-    try {
-        logic.searchDucks(token, query) // cojerá token
-            .then((ducks) => res.json({ ducks }))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
+
+    handleErrors(() =>
+        logic.searchDucks(token, query)
+            .then((ducks) => res.json({ ducks })),
+        res)
 })
 
 app.get('/duckDetail/:id', jsonParser, (req, res) => { // Un get + ruta search
-    let { headers: { authorization: token }, params: { id }} = req
+    let { headers: { authorization: token }, params: { id } } = req
     token = token.split(' ')[1]
-    
-    try {
+
+    handleErrors(() =>
         logic.retrieveDuck(token, id) // cojerá token
-            .then(duck => res.json({ duck }))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
+            .then(duck => res.json({ duck })),
+        res)
 })
 
-app.post('/user/favs/:id', jsonParser, (req, res) => { 
-    let { headers: { authorization: token }, params: { id }} = req
+app.post('/user/favs/:id', jsonParser, (req, res) => {
+    let { headers: { authorization: token }, params: { id } } = req
     token = token.split(' ')[1]
-    
-    try {
+
+    handleErrors(() =>
         logic.toggleFavDuck(token, id)
-            .then(() => res.json({ message: 'Done'}))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
+            .then(() => res.json({ message: 'Done' })),
+        res)
 })
 
-app.get('/user/favs', jsonParser, (req, res) => { 
-    let { headers: { authorization: token }, params: { id }} = req
+app.get('/user/favs', jsonParser, (req, res) => {
+    let { headers: { authorization: token }, params: { id } } = req
     token = token.split(' ')[1]
-    
-    try {
+
+    handleErrors(() =>
         logic.retrieveFavDucks(token)
-            .then(ducks => res.json({ ducks }))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
+            .then(ducks => res.json({ ducks })),
+        res)
 })
 
 app.use(function (req, res, next) {
